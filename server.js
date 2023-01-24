e4e1d9a9999ea6f1deefadd3f28a6aead1921d2d
Mousam Panthi <mousampanthi67@gmail.com>
2023-01-24 19:00:39 +0530

    changed host

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image')
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgres://smart_brain_database_0mnu_user:cDG6FnrJhR9cTI2Hxg90vHyuxn3u8ZUZ@dpg-cf7svaun6mplr402v310-a/smart_brain_database_0mnu',
+    host : 'dpg-cf7svaun6mplr402v310-a',
     port : 5432,
     user : 'smart_brain_database_0mnu_user',
     password : 'cDG6FnrJhR9cTI2Hxg90vHyuxn3u8ZUZ',
