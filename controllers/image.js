5e4b104ad1b80690ce881f5aabd73603aaf0263c
Mousam Panthi <mousampanthi67@gmail.com>
2023-01-24 16:55:58 +0530

    adding project files

---
+++ controllers/image.js
@@ -0,0 +1,65 @@
+const Clarifai = require('clarifai');
+
+const handleApiCall = (req, res) => {
+	const USER_ID = "mousam_panthi";
+    const PAT = "01c264f80f0f4474b44bec85153f8fdb";
+    const APP_ID = "my-first-application"; 
+    const MODEL_ID = "face-detection";
+    const MODEL_VERSION_ID = "45fb9a671625463fa646c3523a3087d5";
+    const IMAGE_URL = req.body.input; 
+    const raw = JSON.stringify({
+        user_app_id: {
+          user_id: USER_ID,
+          app_id: APP_ID,
+        },
+        inputs: [
+          {
+            data: {
+              image: {
+                url: IMAGE_URL,
+              },
+            },
+          },
+        ],
+      });
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          Authorization: "Key " + PAT,
+        },
+        body: raw,
+      };
+
+    fetch(
+        "https://api.clarifai.com/v2/models/" +
+          MODEL_ID +
+          "/versions/" +
+          MODEL_VERSION_ID +
+          "/outputs",
+        requestOptions
+    )
+    .then((response) => response.json())
+    .then(data => {
+    	res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+	
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+	db('users').where('id', '=', id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0].entries);
+	})
+	.catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+	handleImage,
+	handleApiCall
+}
